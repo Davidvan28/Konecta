033b6cc21c7300ab00a931c9f485e427b57c8319
Davidvan28 <117795016+Davidvan28@users.noreply.github.com>
2022-11-08 23:52:32 -0500

    v1

--- includes/crear.php
+++ includes/crear.php
@@ -39,7 +39,7 @@ include_once("db.php");
          $datosI->closeCursor(); 
 	     $datosI = null; 
  	}
-
+ //funcion que ayudara a eliminar datos a la table en mysql
  	public function EliminarDatos(string $ID){
       $eliminar = $this->connect()->prepare("DELETE FROM `productos` WHERE ID = :id");
       $eliminar->bindParam(":id", $ID);
@@ -48,7 +48,7 @@ include_once("db.php");
       $eliminar->closeCursor(); 
 	  $eliminar = null; 
  	}
-
+//funcion que ayudara a cargar datos de la table en mysql
  	public function CargarInfo(string $codigo){
  		$conectar = $this->connect()->prepare("SELECT `ID`, `Nombre_Producto`, `Referencia`, `Precio`, `Peso`, `Categoria`, `Stock`, `Fecha_Creacion` FROM `productos`".$codigo."");
         $conectar->execute();
@@ -59,6 +59,7 @@ include_once("db.php");
 	 $conectar = null; 
  	}
 
+  //funcion que ayudara a editar datos de la table en mysql
  	public function EditarDatos(string $nombre, string $referencia, int $precio, int $peso, string $categoria, int $stock, int $ID){
          $this->$ID = $ID;
  		 $this->$nombre = $nombre;
@@ -69,13 +70,6 @@ include_once("db.php");
          $this->$stock = $stock;
        $Editar = $this->connect()->prepare("UPDATE `productos` SET `Nombre_Producto`= ?,`Referencia`= ?,`Precio`= ?,`Peso`= ?,`Categoria`= ?,`Stock`= ? WHERE ID = ?");
        $arrayE = array($this->$nombre,$this->$referencia, $this->$precio, $this->$peso, $this->$categoria, $this->$stock, $ID );
-       // $Editar->bindParam(":nombre", $nombre);
-       // $Editar->bindParam(":referencia", $referencia);
-       // $Editar->bindParam(":precio", $precio);
-       // $Editar->bindParam(":peso", $peso);
-       // $Editar->bindParam(":categoria", $categoria);
-       // $Editar->bindParam(":stock", $stock);
-       // $Editar->bindParam(":id", $ID);
        $Editar->execute($arrayE);
        return $Editar;
        $Editar->closeCursor(); 
